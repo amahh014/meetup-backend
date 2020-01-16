84bb7f2fee36fa6240bd61b3c86045d6155879df
amahh014 <nizammamedovv@gmail.com>
2020-01-16 02:30:40 +0100

    connected to MongoDb and basic connection

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dbConfig from './config/db';
+
+const app = express();
+
+dbConfig();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+    if(err) {
+        console.log(err);
+    } {
+        console.log(`App listen to port ${PORT}`)
+    }
+});
