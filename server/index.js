d8bb240b917bf68a84bccbbdfb452c6ca9f2db5c
amahh014 <nizammamedovv@gmail.com>
2020-01-17 02:28:41 +0100

    Schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import dbConfig from './config/db';
+import middlewaresConfig from './config/middlewares';
+import { MeetupRoutes } from './modules';
 
 const app = express();
 
 dbConfig();
+
+middlewaresConfig(app);
+
+app.use('/api', [MeetupRoutes]);
 
 const PORT = process.env.PORT || 3000;
 
