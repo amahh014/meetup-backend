5c69e4018bf190eefb7d8ebb24ad552082cc4629
amahh014 <nizammamedovv@gmail.com>
2020-01-21 14:37:02 +0100

    Get group meetups

--- server/modules/groups/model.js
+++ server/modules/groups/model.js
@@ -24,18 +24,14 @@ const GroupSchema = new Schema({
 GroupSchema.statics.addMeetup = async function (id, args) {
   const Meetup = mongoose.model('Meetup');
 
-  const group = await this.findById(id);
+  const meetup = await new Meetup({ ...args, group: id });
 
-  const meetup = await new Meetup({ ...args, group });
+  const group = await this.findByIdAndUpdate(id, { $push: {meetups: meetup.id} });
 
-  group.meetups.unshift(meetup);
-
-  // console.log(group)
-
-  const result = await Promise.all([meetup.save(), group.save()]);
-
-  //console.log(result)
-  return result;
+  return {
+    meetup: await meetup.save(),
+    group
+  };
 };
 
 export default mongoose.model('Group', GroupSchema);
